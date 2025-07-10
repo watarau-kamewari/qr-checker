3b1100cd333e173092545a7c1ca363b5c232575b
wataru kamewari <kamewari@naganosankoh.jp>
2025-07-10 20:24:03 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,7 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+      .then(() => console.log('✅ SW登録成功'))
+      .catch(err => console.error('❌ SW登録失敗:', err));
+  }
+</script>
